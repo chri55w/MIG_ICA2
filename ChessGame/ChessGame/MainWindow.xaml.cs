152cc5cda84d813d02d6a5f4abc0967b5aae93bc
chri55w <chri5walters@msn.com>
2015-03-30 18:51:45 +0100

    User Viewer Working

--- ChessGame/ChessGame/MainWindow.xaml.cs
+++ ChessGame/ChessGame/MainWindow.xaml.cs
@@ -29,7 +29,6 @@ namespace ChessGame {
         public MainWindow() {
             InitializeComponent();
             Loaded += OnLoaded;
-
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs) {
@@ -68,52 +67,18 @@ namespace ChessGame {
                         args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
                         error = true;
                     }
-                } catch (InvalidOperationException) {
+                }
+                catch (InvalidOperationException)
+                {
                     error = true;
                     // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
                     // E.g.: sensor might be abruptly unplugged.
                 }
             }
 
-            if (!error)
+            if (!error) {
                 kinectRegion.KinectSensor = args.NewSensor;
-        }
-        /*
-        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args) {
-            MessageBox.Show(args.NewSensor == null ? "No Kinect" : args.NewSensor.Status.ToString());
-        }
-        private static void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args) {
-            if (args.OldSensor != null) {
-                try {
-                    args.OldSensor.DepthStream.Range = DepthRange.Default;
-                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
-                    args.OldSensor.DepthStream.Disable();
-                    args.OldSensor.SkeletonStream.Disable();
-                } catch (InvalidOperationException) {
-                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
-                    // E.g.: sensor might be abruptly unplugged.
-                }
-            }
-
-            if (args.NewSensor != null) {
-                try {
-                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                    args.NewSensor.SkeletonStream.Enable();
-
-                    try {
-                        args.NewSensor.DepthStream.Range = DepthRange.Near;
-                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
-                    } catch (InvalidOperationException) {
-                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
-                        args.NewSensor.DepthStream.Range = DepthRange.Default;
-                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
-                    }
-                } catch (InvalidOperationException) {
-                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
-                    // E.g.: sensor might be abruptly unplugged.
-                }
             }
         }
-        */
     }
 }
